3bb88a102a2109d15db8de97f5c97a05fba674fe
Ken Ling <ken88ling@gmail.com>
2017-05-04 13:11:03 +0800

    Add user and password

--- IdentityV2/Program.cs
+++ IdentityV2/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace IdentityV2
 {
@@ -10,7 +14,23 @@ namespace IdentityV2
     {
         static void Main(string[] args)
         {
+            var username = "ken@ken.com";
+            var password = "password";
 
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+            //var createResult = userManager.Create(new IdentityUser("ken@ken.com"), "password");
+            //Console.WriteLine("created : {0}",createResult.Succeeded);
+
+            IdentityUser user = userManager.FindByName(username);
+            var claimResult = userManager.AddClaim(user.Id, new Claim("give_type", "give_value"));
+            //Console.WriteLine("Claim:{0}", claimResult.Succeeded);
+            //Console.ReadLine();
+
+            var isMatch = userManager.CheckPassword(user, password);
+            Console.WriteLine("Password Match: {0}", isMatch);
+            Console.ReadLine();
         }
     }
 }
